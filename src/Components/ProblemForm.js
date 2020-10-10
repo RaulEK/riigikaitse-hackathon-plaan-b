ffe62df1dc2120ef2ec7869eed49e65830f7fc96
RaulEK <kattai.raul@gmail.com>
2020-10-10 19:20:55 +0300

    data entry responsive style

--- src/Components/ProblemForm.js
+++ src/Components/ProblemForm.js
@@ -4,7 +4,6 @@ import TextField from '@material-ui/core/TextField';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import '../styles/index.scss';
-import Button from "@material-ui/core/Button";
 import InputLabel from "@material-ui/core/InputLabel";
 import withStyles from "@material-ui/core/styles/withStyles";
 import moment from 'moment';
@@ -26,7 +25,7 @@ const StyledTextField = withStyles({
 })(TextField);
 
 const backgroundStyle = "m-auto bg-gray-300 h-full";
-const flexboxContainerStyle = "flex pt-10 px-16 flex-col items-center content-center bg-white w-2/3 h-full m-auto rounded-lg shadow-xl";
+const flexboxContainerStyle = "flex pt-10 px-16 flex-col items-center content-center bg-white w-full lg:w-2/3 h-full m-auto rounded-lg shadow-xl";
 
 const ProblemForm = () => {
     const classes = useStyles();
@@ -51,27 +50,28 @@ const ProblemForm = () => {
         <div className={CONTENTSTYLE + "w-full h-full bg-gray-200 p-10"}>
             <div className={flexboxContainerStyle}>
                 <div className="flex w-full">
-                    <h1 className="text-2xl">Taotlus</h1>
+                    <h1 className="text-2xl">Arstivisiidi taotlus</h1>
                 </div>
                 <div className="mb-3 mt-3 w-full">
                     <TextField className={StyledTextField} onChange={(event) => setName(event.target.value)}
                                id="standard-basic" label="Nimi"
                                fullWidth
-                               value={name} required/>
+                               value={name} required
+                               inputProps={{style: {fontSize: 20}}}/>
                 </div>
                 <div className="mb-3 mt-3 w-full">
                     <TextField onChange={(event) => setSymptoms(event.target.value)}
                                id="standard-basic" label="Mis on sümptomid?"
-                               fullWidth multiline value={symptoms} rows={4} variant="outlined" required/>
+                               fullWidth multiline value={symptoms} rows={4} variant="outlined" required inputProps={{style: {fontSize: 20}}}/>
                 </div>
                 <div className="mb-3 mt-3 w-full">
                     <TextField onChange={(event) => setDescription(event.target.value)}
                                id="standard-basic" label="Miks tervisehäda tekkis?" fullWidth
-                               multiline value={description} rows={4} variant="outlined" required/>
+                               multiline value={description} rows={4} variant="outlined" required inputProps={{style: {fontSize: 20}}}/>
                 </div>
 
                 <div className="flex justify-around w-full h-1/2 mt-12 items-start">
-                    <div className="flex flex-col w-1/3">
+                    <div className="flex flex-col w-2/5 lg:w-1/3">
                         <InputLabel>Millal tervisehäda tekkis?</InputLabel>
                         <TextField
                             required
@@ -90,9 +90,9 @@ const ProblemForm = () => {
                             : <button onClick={() => setSkipDate(null)}>Tühista valik</button>}
                     </div>
 
-                    <Button onClick={() => handleSend()} variant="contained" color="primary" className="h-12">
+                    <button onClick={() => handleSend()} className="text-md lg:text-xl py-1 rounded bg-blue-400 text-white h-2/3 w-40 lg:w-26 display justify-center ml-4 shadow-lg hover:bg-blue-500">
                         Saada
-                    </Button>
+                    </button>
                 </div>
             </div>
         </div>
