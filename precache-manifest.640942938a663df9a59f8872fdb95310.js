d78a443514cbd460a4eaa8d9ef0e044745ae6093
RaulEK <kattai.raul@gmail.com>
2020-10-10 16:02:43 +0300

    Updates

---
+++ precache-manifest.640942938a663df9a59f8872fdb95310.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f757ff8b66c2529f95ce80b10b8d279",
+    "url": "/riigikaitse-hackathon-plaan-b/index.html"
+  },
+  {
+    "revision": "0c980aa20d488eaad9f4",
+    "url": "/riigikaitse-hackathon-plaan-b/static/css/2.85dfc17e.chunk.css"
+  },
+  {
+    "revision": "2e9fa7a6686ce7dfde5a",
+    "url": "/riigikaitse-hackathon-plaan-b/static/css/main.d6928b13.chunk.css"
+  },
+  {
+    "revision": "0c980aa20d488eaad9f4",
+    "url": "/riigikaitse-hackathon-plaan-b/static/js/2.2a03f9fd.chunk.js"
+  },
+  {
+    "revision": "3d082645dfe23f5c1ffa73f047f1c38b",
+    "url": "/riigikaitse-hackathon-plaan-b/static/js/2.2a03f9fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e9fa7a6686ce7dfde5a",
+    "url": "/riigikaitse-hackathon-plaan-b/static/js/main.74d097f4.chunk.js"
+  },
+  {
+    "revision": "badf7393110a68b41289",
+    "url": "/riigikaitse-hackathon-plaan-b/static/js/runtime-main.fb22f83a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/riigikaitse-hackathon-plaan-b/precache-manifest.8d454f067524480a40e869740cddfe91.js"
+  "/riigikaitse-hackathon-plaan-b/precache-manifest.640942938a663df9a59f8872fdb95310.js"
 );
 
 self.addEventListener('message', (event) => {
