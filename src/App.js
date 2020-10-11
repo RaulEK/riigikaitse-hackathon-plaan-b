f5a403f20ebc29377f846b9f58789dae54e5a271
Krister Looga <looga.krister@gmail.com>
2020-10-11 09:30:32 +0300

    added hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Switch} from "react-router-dom";
+import {HashRouter, Route, Switch} from "react-router-dom";
 import LandingPage from "./Components/Landingpage";
 import Doctor from "./Containers/Doctor";
 import Header from "./Components/Header";
@@ -9,31 +9,33 @@ import QuestionForm from "./Components/QuestionForm";
 
 function App() {
     return (
-        <div className="h-screen">
-            <Header/>
-            <div className="pt-24 h-full">
-                <Switch>
-                    <Route path="/form">
-                        <ProblemForm/>
-                    </Route>
-                    <Route path="/kalender">
-                        <CalendarView/>
-                    </Route>
-                    <Route path="/arst">
-                        <Doctor/>
-                    </Route>
-                    <Route path="/question">
-                        <QuestionForm />
-                    </Route>
-                    <Route path="/">
-                        <LandingPage/>
-                    </Route>
-                    <Route path="/kalender">
-                        <CalendarView/>
-                    </Route>
-                </Switch>
+        <HashRouter basename='/'>
+            <div className="h-screen">
+                <Header/>
+                <div className="pt-24 h-full">
+                    <Switch>
+                        <Route path="/form">
+                            <ProblemForm/>
+                        </Route>
+                        <Route path="/kalender">
+                            <CalendarView/>
+                        </Route>
+                        <Route path="/arst">
+                            <Doctor/>
+                        </Route>
+                        <Route path="/question">
+                            <QuestionForm />
+                        </Route>
+                        <Route path="/">
+                            <LandingPage/>
+                        </Route>
+                        <Route path="/kalender">
+                            <CalendarView/>
+                        </Route>
+                    </Switch>
+                </div>
             </div>
-        </div>
+        </HashRouter>
     );
 }
 
